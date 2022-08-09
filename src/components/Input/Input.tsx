e09e79b2117afa9b09e9c905136ef6594d2a376a
Dênis Luft <denisluft8@gmail.com>
2022-08-09 19:39:39 -0300

    altered colors

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,20 +10,23 @@ interface InputProps {
 }
 
 const InputStyled = styled.input<InputProps>`
-  background: ${colors["faux-smoky-grey"]};
+  background: ${(props) => props.theme.colors.background};
   box-shadow: inset 3px 3px 10px rgba(0, 0, 0, 0.2),
     inset -1px -1px 0px rgba(255, 255, 255, 0.3);
   border-radius: 10px;
   backdrop-filter: blur(20px);
   height: ${(props) => props.height};
   width: ${(props) => props.width};
+  padding-left: 10px;
+  color: ${(props) => props.theme.colors.primary};
+  border: none;
 
   &::placeholder {
-    color: ${colors["light-silver"]};
+    color: ${(props) => props.theme.colors.secondary};
     font-size: 16px;
     font-weight: ${fontSize["body"]};
     line-height: ${lineHeight["body"]};
-    padding-left: 10px;
+    
   }
 `;
 
